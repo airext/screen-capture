bfa70dcfc70ef8b939cd78636edaccb02ff7cec2
mrozdobudko <max.rozdobudko@gmail.com>
2012-04-06 10:20:14 +0300

    Adds "build/extensions" folder

--- build/build_win.bat
+++ build/build_win.bat
@@ -3,4 +3,4 @@ echo Building DLL (shared library for windows) file...
 set MicrosoftSDK=C:/Program Files/Microsoft SDKs/Windows/v7.0A
 
 gcc -O3 -Wall -c -fmessage-length=0 -o ../win/Release/ScreenCapture.o ../win/ScreenCapture.c
-gcc -shared -o ScreenCapture.dll ../win/Release/ScreenCapture.o ../win/libs/FlashRuntimeExtensions.lib "%MicrosoftSDK%/Lib/Gdi32.Lib"
+gcc -shared -o ScreenCapture.dll ../win/Release/ScreenCapture.o ../win/libs/FlashRuntimeExtensions.lib ../win/libs/Gdi32.Lib
